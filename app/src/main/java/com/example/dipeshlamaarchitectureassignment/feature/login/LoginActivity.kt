97a6574b3447f4edbc26988f18ab5b8e1bd44b2b
DipeshLama <dipeslama32@gmail.com>
2022-03-22 18:21:22 +0545

    added comments

--- app/src/main/java/com/example/dipeshlamaarchitectureassignment/feature/login/LoginActivity.kt
+++ app/src/main/java/com/example/dipeshlamaarchitectureassignment/feature/login/LoginActivity.kt
@@ -37,6 +37,12 @@ class LoginActivity : MvpActivity<LoginView,LoginPresenter>(),LoginView ,View.On
     }
 
     override fun loginUser(success: LoginResponse?) {
+        /*
+        The email and password for the login is dipesh32@gmail.com and 123456
+        Since my URL is http://demo3147732.mockable.io/login?email=dipesh32@gmail.com&password=123456
+        @Query annotation is used to pass the value(email and password) of edittext in the URL.
+         */
+
         if(success?.success == true){
             startActivity(Intent(this, MovieListActivity::class.java))
             finish()
